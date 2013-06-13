7aa7a2b8aaac7dbfd063a2e96a3ee50f21b0bce9
RD Team <bear.prada@gmail.com>
2013-06-13 14:02:12 +0800

    add linked list

--- TestFunctionPoint/main.c
+++ TestFunctionPoint/main.c
@@ -18,13 +18,90 @@ int jimmy(char op, int (*func)(int, int)) {
     return -1;
 }
 
-void sort(int * array, int length, bool (*cmp)(int a,int b)){
+typedef struct node{
+    int id;
+    struct node * next;
+    struct node * prev;
+}Node;
+
+Node * head;
+Node * tail;
+
+void addNode(Node * n){
+    if(head==NULL){
+        head=n;
+        tail=head;
+    }else{
+        n->prev = tail;
+        tail->next = n;
+        tail = tail->next;
+    }
+}
+
+void delete(int id){
+    Node * p = head;
+    while(p!=NULL){
+        if(p->id==id){
+            p->prev->next = p->next;
+            p->next->prev = p->prev;
+            return;
+        }
+        p = p->next;
+    }
+}
+
+void blub_sort(){
+    Node * p = head;
+    while(p!=NULL){
+        //TODO double loop
+        if(p->next!=NULL){
+        if(p->id < p->next->id ){
+            Node * a = p;
+            Node * b = p->next;
+            Node * tmp = a->prev;
+            a->prev = b;
+            a->next = b->next;
+            b->prev = a;
+            b->next = tmp;
+        }
+        }
+        p = p->next;
+    }
+}
+
+void merge_sort(){
     
 }
 
+void addToBottom(int id){
+    Node * n = (Node *)malloc(sizeof(Node));
+    n->id = id;
+    addNode(n);
+}
+
+
 int main(void) {
-    int (*func)(int, int) = add;
+    /*int (*func)(int, int) = add;
     printf("%d\n", jimmy('+', func));
+    */
+    addToBottom(7);
+    addToBottom(1);
+    addToBottom(2);
+    addToBottom(3);
+    addToBottom(4);
+    addToBottom(5);
+    
+    delete(2);
+    delete(6);
+    
+    blub_sort();
+    
+    Node * p = head;
+    while(p!=NULL){
+        printf("%d ",p->id);
+        p = p->next;
+    }
+    
     return 0;
 }
 
